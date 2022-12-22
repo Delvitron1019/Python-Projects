cf621b6f30d4e09fa822d3e5d222706def01a78d
Dev Shah <96745112+Delvitron1019@users.noreply.github.com>
2022-12-22 20:06:09 +0530

    Update Weather_forecast.py

--- Weather_forecast.py
+++ Weather_forecast.py
@@ -1,13 +1,25 @@
 import requests
+import speech_recognition as sr
 import pyttsx3 as tts
 
+listener = sr.Recognizer()
 engine = tts.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-city = input('Enter the city name: ')
+engine.say('Specify the city name: ')
+engine.runAndWait()
+
+with sr.Microphone() as source:
+        print('recording...')
+        voice = listener.listen(source)
+        city = listener.recognize_google(voice)
+        print(city)
+
 engine.say('Displaying Weather report for ' + city)
 engine.runAndWait()
+
 url = 'https://wttr.in/{}'.format(city)
-res = requests.get(url)
-print(res.text)
+result = requests.get(url)
+
+print(result.text)
